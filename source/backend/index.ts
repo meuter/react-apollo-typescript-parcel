5f41bbcce3217264b7928e068db6d55993a5847b
Cédric Meuter <cedric.meuter@gmail.com>
2019-12-26 14:24:33 +0100

    Backend is now an Apollo server

--- source/backend/index.ts
+++ source/backend/index.ts
@@ -1,7 +1,23 @@
-import express from "express"
+import "reflect-metadata"
+import { ApolloServer } from "apollo-server"
+import { buildSchema, Resolver, Query } from "type-graphql"
 
-(() => {
-    const app = express()
-    app.get("/", (req, res) => res.send("Hello World from Backend"))
-    app.listen({ port: 3000 })
+@Resolver()
+class HelloWorldResolver {
+    @Query(() => String)
+    helloWorld() {
+        return "Hello World"
+    }
+}
+
+(async () => {
+    const PORT = 3000
+    const schema = await buildSchema({
+        resolvers: [HelloWorldResolver]
+    })
+    const server = new ApolloServer({ schema });
+
+    server.listen(PORT).then(({ url }) => {
+        console.log(`Apollo server ready at ${url}`);
+    });
 })()
